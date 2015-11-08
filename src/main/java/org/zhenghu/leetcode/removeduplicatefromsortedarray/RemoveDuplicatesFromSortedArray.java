526cde910cc055116f00e93d7b3eb0a40751372c
zhenghu <zheng.hu@fidelise.fr>
2015-11-09 00:23:12 +0100

    [leetcode]Remove Duplicates from Sorted Array

--- src/main/java/org/zhenghu/leetcode/removeduplicatefromsortedarray/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/org/zhenghu/leetcode/removeduplicatefromsortedarray/RemoveDuplicatesFromSortedArray.java
@@ -1,36 +1,26 @@
 package org.zhenghu.leetcode.removeduplicatefromsortedarray;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Remove Duplicates from sorted array
  */
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0){
-            return 0;
+        if(nums.length <= 2){
+            return nums.length;
         }
 
         Arrays.sort(nums);
 
-        List<Integer> duplications = new ArrayList<>();
-        int duplicate = 0;
-        int compare = nums[0];
+        int count = 2;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (compare == nums[i]) {
-                duplicate++;
-                if (duplicate >= 2) {
-                    duplications.add(nums[i]);
-                }
+        for (int i = 2; i < nums.length; i++) {
+            nums[count] = nums[i];
+            if (!(nums[count] == nums[count-1] && nums[count]==nums[count-2])){
+                count++;
             }
-            if (compare != nums[i]) {
-                duplicate = 0;
-            }
-            compare = nums[i];
         }
-        return nums.length - duplications.size();
+        return count;
     }
 }
