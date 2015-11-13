c0ec3c608158808a06802d0b9836282f078e6aec
ZHENG HU <zheng-externe.hu@erdf.fr>
2015-11-13 18:12:53 +0100

    [leetcode-java]Happy Number not finish

---
+++ src/main/java/org/zhenghu/leetcode/happynumber/HappyNumber.java
@@ -0,0 +1,16 @@
+package org.zhenghu.leetcode.happynumber;
+
+/**
+ * Happy Number Solution
+ */
+public class HappyNumber {
+
+	public boolean isHappy(int n){
+		String numberStr = String.valueOf(n);
+		for(String number : numberStr.split("")){
+
+		}
+		return false;
+	}
+
+}
---
+++ src/test/java/org/zhenghu/leetcode/happynumber/HappyNumberTest.java
@@ -0,0 +1,32 @@
+package org.zhenghu.leetcode.happynumber;
+
+import org.fest.assertions.api.Assertions;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+
+/**
+ * test
+ */
+@RunWith(ZohhakRunner.class)
+public class HappyNumberTest {
+
+	HappyNumber happyNumber;
+
+	@Before
+	public void setUp() throws Exception {
+		this.happyNumber = new HappyNumber();
+	}
+
+	@TestWith({
+			"19,true"
+	})
+	public void testIsHappy(int input, boolean expected) throws Exception {
+		// Given
+        // When
+		// Then
+		Assertions.assertThat(this.happyNumber.isHappy(input)).isEqualTo(expected);
+	}
+}
